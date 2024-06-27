27d94e64e4644005577c7a1ca067f4707b900e26
Aycan <aycan.acn2020@gmail.com>
2024-06-27 14:41:35 +0200

    finish basic front-end look for now

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./style.css";
+
+const url =
+  "http://api.weatherapi.com/v1/forecast.json?key=9502aa35a4e74be39f0170815242106&q=Amsterdam&days=3&aqi=no&alerts=no";
+
+async function getForecastCity(city) {
+  const query = `q=${city}`;
+  const newUrl = url.replace("q=", query);
+
+  const response = await fetch(url, { mode: "cors" });
+  const responseJson = await response.json();
+
+  return responseJson;
+}
